c416b6a59323131e6da9dd4feaa75ec705d81237
Nilli Martinez <52380654+nmsis@users.noreply.github.com>
2019-06-30 13:25:59 -0500

    Create clock100hz.v

---
+++ Biped-Robot/clock100hz.v
@@ -0,0 +1,19 @@
+// 100 Hz Clock to generate PWM
+
+module clock100hz(input clk, output senal);	//es de 50 hz
+reg [19:0] conta;
+reg tempo; //evita que la variable serial sea registro
+
+assign senal=tempo;
+
+always @(posedge clk)
+begin
+	conta=conta+1;
+	if (conta == 20'd5000)
+	begin
+		conta=0;
+		tempo = ~tempo;
+		
+	end
+end
+endmodule
