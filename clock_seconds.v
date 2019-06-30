82228556af17b0cc67b87fd5ac78dcfc182d95ac
Nilli Martinez <52380654+nmsis@users.noreply.github.com>
2019-06-30 12:56:33 -0500

    Create clock_seconds.v

---
+++ clock_seconds.v
@@ -0,0 +1,17 @@
+//This program creates a clock counter for 1 second
+
+module clock_seconds (input clk, output reg led);
+reg[24:0] contador;
+
+always @(posedge clk)
+	begin
+		//if(contador == 25'd24999999 )
+		if(contador == 25'd9 )
+			begin
+				contador=0;
+				led = ~led;
+			end
+		contador=contador+1;
+	end
+
+endmodule 
